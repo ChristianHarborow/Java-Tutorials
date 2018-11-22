4bff698ce2b45cdc99f2c7c3e4ddabcfbd4b88c2
Christian Harborow <christianharborow@hotmail.co.uk>
2018-11-22 19:49:55 +0000

    Added Hello and Employee classes

---
+++ src/Week08/Hello.java
@@ -0,0 +1,19 @@
+package Week08;
+
+public class Hello {
+
+    public Hello ()
+    {
+        sayHello ();
+    }
+
+    private void sayHello ()
+    {
+        System.out.println ("hello, world");
+    }
+
+    public static void main (String args[])
+    {
+        new Hello ();
+    }
+}
