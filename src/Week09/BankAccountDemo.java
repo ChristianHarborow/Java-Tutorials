14673b5246081b3416b2a3b46da553caf6f5743b
Christian Harborow <christianharborow@hotmail.co.uk>
2018-11-29 22:27:50 +0000

    Added BankAccountOptional class and updated BankAccount and BankAccountDemo

--- src/Week09/BankAccountDemo.java
+++ src/Week09/BankAccountDemo.java
@@ -1,15 +1,37 @@
 package Week09;
 
-public class BankAccountDemo
-{
-    public static void main (String[] args)
-    {
-        BankAccount account = new BankAccount("117", "John Smith", true);
-        account.withdraw(100.5);
+public class BankAccountDemo {
+    public static void main (String[] args) {
+        BankAccount account = new BankAccount("1234", "John Smith", false);
         System.out.println(account);
-        account.deposit(1037.75);
-        System.out.println(account);
-        account.addInterest(5);
-        System.out.println(account);
+        //testing over withdrawal condition
+        if (account.withdraw(100.00)) {
+            System.out.println("Withdrawal successful");
+        }
+        else {
+            System.out.println("Withdrawal failed");
+        }
+        account.setHasOverdraft(true);
+        //testing withdrawal overdraft condition
+        if (account.withdraw(100.00)) {
+            System.out.println("Withdrawal successful");
+        }
+        else {
+            System.out.println("Withdrawal failed");
+        }
+        //testing interest negative balance condition
+        if (account.addInterest(5)) {
+            System.out.println("Interest added");
+        }
+        else {
+            System.out.println("Interest could not be payed");
+        }
+        //testing negative deposit condition
+        if (account.deposit(-150.00)) {
+            System.out.println("Amount deposited");
+        }
+        else {
+            System.out.println("Unable to deposit amount");
+        }
     }
 }
