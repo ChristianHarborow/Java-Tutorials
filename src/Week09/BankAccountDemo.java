d7e7598c2f47f9cbe04f8764a2ded8418fd4f583
Christian Harborow <christianharborow@hotmail.co.uk>
2018-12-03 20:57:04 +0000

    Updated bankAccountDemo with additional cases

--- src/Week09/BankAccountDemo.java
+++ src/Week09/BankAccountDemo.java
@@ -4,34 +4,62 @@ public class BankAccountDemo {
     public static void main (String[] args) {
         BankAccount account = new BankAccount("1234", "John Smith", false);
         System.out.println(account);
-        //testing over withdrawal condition
+        //testing withdrawal with no overdraft case
+        System.out.println("Test 1 [Expecting Fail]\nAttempting withdrawal...");
         if (account.withdraw(100.00)) {
-            System.out.println("Withdrawal successful");
+            System.out.println("Withdrawal successful\n");
         }
         else {
-            System.out.println("Withdrawal failed");
+            System.out.println("Withdrawal failed\n");
         }
         account.setHasOverdraft(true);
-        //testing withdrawal overdraft condition
+        //testing withdrawal with overdraft case
+        System.out.println("Test 2 [Expecting Success]\nAttempting withdrawal...");
         if (account.withdraw(100.00)) {
-            System.out.println("Withdrawal successful");
+            System.out.println("Withdrawal successful\n");
         }
         else {
-            System.out.println("Withdrawal failed");
+            System.out.println("Withdrawal failed\n");
         }
-        //testing interest negative balance condition
+        //testing interest on negative balance case
+        System.out.println("Test 3 [Expecting Fail]\nCalculating interest...");
         if (account.addInterest(5)) {
-            System.out.println("Interest added");
+            System.out.println("Interest added\n");
         }
         else {
-            System.out.println("Interest could not be payed");
+            System.out.println("Interest could not be payed\n");
         }
-        //testing negative deposit condition
+        //testing negative deposit case
+        System.out.println("Test 4 [Expecting Fail]\nAttempting deposit...");
         if (account.deposit(-150.00)) {
-            System.out.println("Amount deposited");
+            System.out.println("Amount deposited\n");
         }
         else {
-            System.out.println("Unable to deposit amount");
+            System.out.println("Unable to deposit amount\n");
+        }
+        //testing valid deposit case
+        System.out.println("Test 5 [Expecting Success]\nAttempting deposit...");
+        if (account.deposit(150.00)) {
+            System.out.println("Amount deposited\n");
+        }
+        else {
+            System.out.println("Unable to deposit amount\n");
+        }
+        //testing negative interest rate case
+        System.out.println("Test 6 [Expecting Fail]\nCalculating interest...");
+        if (account.addInterest(-5)) {
+            System.out.println("Interest added\n");
+        }
+        else {
+            System.out.println("Interest could not be payed\n");
+        }
+        //testing valid interest case
+        System.out.println("Test 7 [Expecting Success]\nCalculating interest...");
+        if (account.addInterest(5)) {
+            System.out.println("Interest added\n");
+        }
+        else {
+            System.out.println("Interest could not be payed\n");
         }
     }
 }
